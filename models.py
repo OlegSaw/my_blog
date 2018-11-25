b236ef261648f651a44b1da869d188b656df6569
OlegSaw <oleg-saw@yandex.ru>
2018-11-25 21:11:50 +0300

    add tag model

--- models.py
+++ models.py
@@ -4,40 +4,46 @@ import re
 
 
 def slugify(s):
-	pattern = r'[^\w+]'
-	return re.sub(pattern, '-', s)
+    pattern = r'[^\w+]'
+    return re.sub(pattern, '-', s)
+
+
+post_tags = db.Table('post_tags', db.Column('post_id', db.Integer, db.ForeignKey('posts.id')),
+                     db.Column('tag_id', db.Integer, db.ForeignKey('tags.id')))
 
 
 class Post(db.Model):
-	__tablename__ = 'posts'
-	id = db.Column(db.Integer, primary_key=True)
-	title = db.Column(db.String(148))
-	slug = db.Column(db.String(148), unique=True)
-	body = db.Column(db.Text)
-	created = db.Column(db.DateTime, default=datetime.now())
-	
-	def __init__(self, *args, **kwargs):
-		super(Post, self).__init__(*args, **kwargs)
-		self.generate_slug()
-		
-	def generate_slug(self):
-		if self.title:
-			self.slug = slugify(self.title)
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(148))
+    slug = db.Column(db.String(148), unique=True)
+    body = db.Column(db.Text)
+    created = db.Column(db.DateTime, default=datetime.now())
 
-	def __repr__(self):
-		return '<Post id: {}, title {},'.format(self.id, self.title)
+    def __init__(self, *args, **kwargs):
+        super(Post, self).__init__(*args, **kwargs)
+        self.generate_slug()
+
+    tags = db.relationship('Tag', secondary=post_tags, backref=db.backref('posts', lazy='dynamic'))
+
+    def generate_slug(self):
+        if self.title:
+            self.slug = slugify(self.title)
+
+    def __repr__(self):
+        return '<Post id: {}, title {},'.format(self.id, self.title)
 
 
 class Tag(db.Model):
-	__tablename__ = 'tags'
+    __tablename__ = 'tags'
 
-	id = db.Column(db.Integer, primary_key=True)
-	name = db.Column(db.String(100))
-	slug = db.Column(db.String(100))
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    slug = db.Column(db.String(100))
 
-	def __init__(self, *args, **kwargs):
-		super(Tag, self).__init__(*args, **kwargs)
-		self.slug = slugify(self.name)
+    def __init__(self, *args, **kwargs):
+        super(Tag, self).__init__(*args, **kwargs)
+        self.slug = slugify(self.name)
 
-	def __repr__(self):
-		return '<Tag id: {}, name: {}, slug: {}'.format(self.id, self.name, self.slug)
+    def __repr__(self):
+        return '<Tag id: {}, name: {}, slug: {}'.format(self.id, self.name, self.slug)
