3d3f4d2c0deaf96c18c8191518c97771f41cefce
OlegSaw <oleg-saw@yandex.ru>
2018-12-02 22:27:14 +0300

    update search - it WORK!!! on all pages

--- view.py
+++ view.py
@@ -4,24 +4,8 @@ from posts.blueprint import Post
 
 @app.route('/')
 def index():
-    q = request.args.get('q')
-    page = request.args.get('page')
+    return render_template('index.html')
 
-    if page and page.isdigit():
-        page = int(page)
-    else:
-        page = 1
-
-    print("qweqwe: ", page)
-
-    if q:
-        posts = Post.query.filter(Post.title.contains(q) | Post.body.contains(q))
-    else:
-        posts = Post.query.order_by(Post.created.desc())
-
-    pages = posts.paginate(page=page, per_page=5)
-    #return render_template('index.html')
-    return render_template('posts/index.html', posts=posts, pages=pages)
 
 
 @app.errorhandler(404)
